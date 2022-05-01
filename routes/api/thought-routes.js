e8175929aaf3f577e60c5d2c811ad9e9b3be5b9f
jclos305 <jclos305@hotmail.com>
2022-05-01 15:41:44 -0400

    created thought-routes api

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thought-controller');
+
