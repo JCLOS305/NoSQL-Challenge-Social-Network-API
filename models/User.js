2c57dcd2bbaafb5f940f90038d3bff4689673d20
jclos305 <jclos305@hotmail.com>
2022-05-01 15:34:17 -0400

    created User.js model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+// user.js is created
+
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// creates  User model using the UserSchema
+const User = model('User', UserSchema);
+
+// exports  User model
+module.exports = User;
